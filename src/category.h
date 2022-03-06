abe5891d677b639126cb8aed9c340068b96c6612
Cal <callum.adair@outlook.com>
2022-03-06 22:48:20 +0000

    Initial writing of category.h file.

--- src/category.h
+++ src/category.h
@@ -13,8 +13,34 @@
 #ifndef CATEGORY_H
 #define CATEGORY_H
 
-class Category {
+#include <string>
+#include "item.h"
 
+class Category {
+    std::string identifier;
+    std::unordered_map<std::string, Item> items;
+public:
+    Category(const std::string identifier);
+
+    unsigned int size();
+
+    bool empty();
+
+    std::string getIdent();
+
+    void setIdent(std::string identifier);
+
+    Item &newItem(const std::string &item_identifier);
+
+    bool addItem(const Item &item);
+
+    Item &getItem(const std::string &item_identifier);
+
+    bool deleteItem(const std::string &item_identifier);
+
+    friend bool operator==(const Category &lhs, const Category &rhs);
+
+    std::string str();
 };
 
 #endif // CATEGORY_H
