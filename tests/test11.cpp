02b5cd3e2155ff771c551ed24e7525d328134728
cal <callum.adair@outlook.com>
2022-12-23 18:10:07 +0000

    created the skeleton test for verifying json parsing was correct.

---
+++ tests/test11.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Callum Adair on 23/12/22.
+//
+
+#include "../src/lib_catch.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "../src/lib_cxxopts.hpp"
+#include "../src/lib_cxxopts_argv.hpp"
+
+#include "../src/371pass.h"
