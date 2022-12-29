108c4508529d1efaf1d3af9bc2f3858f957e6417
cal <callum.adair@outlook.com>
2022-12-29 19:24:10 +0000

    added basic file prep for the json test file.

--- tests/test11.cpp
+++ tests/test11.cpp
@@ -12,3 +12,22 @@
 #include "../src/lib_cxxopts_argv.hpp"
 
 #include "../src/371pass.h"
+
+
+SCENARIO(
+        "The database and action program arguments can be parsed correctly such that a file can be opened, read and data updated.") {
+
+    const std::string filePath = "./tests/testdatabasethirdalt.json";
+
+    auto fileExists = [](const std::string &path) {
+        return std::ifstream(path).is_open();
+    };
+
+    auto writeFileContents = [](const std::string &path,
+                                const std::string &contents) {
+        // Not a robust way to do this, but here it doesn't matter so much, if it
+        // goes wrong we'll fail the test anyway…
+        std::ofstream f{path};
+        f << contents;
+    };
+}
